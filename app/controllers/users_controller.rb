d713131b63e26d5b1ca57ee9bce83ce1d76f4ba3
olgadraber <olgadraber@gmail.com>
2014-05-14 13:54:09 +0200

    Exposed sorted weekly users
    [#71]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   expose_decorated(:users) { User.order(supports_count: :desc) }
+  expose_decorated(:weekly_users, decorator: UserDecorator) { get_weekly }
   expose_decorated(:user) { User.find params[:id] }
   expose_decorated(:supports) { user.supports.done }
   expose_decorated(:skills) { user.skills.includes(:topic) }
+
+  def get_weekly
+    users.sort_weekly
+  end
+
 end
