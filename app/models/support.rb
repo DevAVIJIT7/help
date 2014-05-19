0f073920e9433fc1d42986356b14596d0398fe11
olgadraber <olgadraber@gmail.com>
2014-05-19 16:37:59 +0200

    Added filter scopes in support

--- app/models/support.rb
+++ app/models/support.rb
@@ -9,6 +9,11 @@ class Support < ActiveRecord::Base
   scope :not_done, -> { where(done: false) }
   default_scope -> { order(updated_at: :desc) }
 
+  scope :filter_by_topic, -> (topic) { joins(:topic).where(topics: {title: topic} ) }
+  scope :filter_by_problem, -> (problem) { where(body: problem) }
+  scope :filter_by_receiver, -> (receiver) { joins(:receiver).where(users: {first_name: receiver} ) and joins(:receiver).where(users: {last_name: receiver} ) }
+  scope :filter_by_user, -> (user) { joins(:user).where(users: {first_name: user} ) and joins(:user).where(users: {first_name: user} ) }
+
   def discussed?
     comments_count > 0
   end
