36816f7677bfbd0fe9d04c8ea5dfecc7f2e66ed0
olgadraber <olgadraber@gmail.com>
2014-05-19 16:42:34 +0200

    Fixed support decorator-gem will_paginate conflict

--- app/decorators/support_decorator.rb
+++ app/decorators/support_decorator.rb
@@ -1,7 +1,8 @@
-class SupportDecorator < Draper::Decorator
+class SupportDecorator < Draper::CollectionDecorator
 
-  decorates :support
+  #decorates :support
   delegate :done?, :body, :discussed?, :receiver, :user, :updated_at
+  delegate :current_page, :per_page, :offset, :total_entries, :total_pages
 
   def topic
     "#{topic.title}"
