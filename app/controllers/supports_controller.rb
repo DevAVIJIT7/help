b4c373a042031ef17e763521391dcc94494acaba
olgadraber <olgadraber@gmail.com>
2014-05-20 13:35:34 +0200

    Edited filtering mechanic in supports controller

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,38 +1,18 @@
 class SupportsController < ApplicationController
 
-  expose_decorated(:topic) { Topic.find(params[:topic_id]) }
-  expose_decorated(:support) { Support.find(params[:id]) }
+  expose_decorated(:topic)    { Topic.find(params[:topic_id]) }
+  expose_decorated(:support)  { Support.find(params[:id]) }
   expose_decorated(:comments) { support.comments.includes(:user).order('created_at ASC') }
-  expose_decorated(:supports) { filtered_supports.paginate(page: params[:page], per_page: 20) }
 
-  expose(:filtered_supports)   { prefilter }
-  expose(:search_params) { params[:query] }
+  expose_decorated(:supports, decorator: SupportCollectionDecorator) do
+    supports_filtered.paginate(page: params[:page], per_page: 20)
+  end
 
-  def prefilter
-    supports = Support.all
-    if search_params
-      case search_params[:search_done]
-      when 'done'
-        supports = Support.done
-      when 'notdone'
-        supports = Support.not_done
-      else
-        supports = Support.all
-      end
-      if search_params[:search_topic]!= ""
-        supports = supports.filter_by_topic(search_params[:search_topic])
-      end
-      if search_params[:search_problem]!=""
-        supports = supports.filter_by_problem(search_params[:search_problem])
-      end
-      if search_params[:search_receiver]!=""
-        supports = supports.filter_by_receiver(search_params[:search_receiver])
-      end
-      if search_params[:search_user]!=""
-        supports = supports.filter_by_user(search_params[:search_user])
-      end
-    end
-    return supports
+  expose(:supports_filtered) { filter_input(filter_state) }
+  expose(:search_params)     { params[:query] }
+
+  %i(search_topic search_problem search_receiver search_user search_state).each do |name|
+    expose(name) { search_params.present? ? search_params[name] : nil }
   end
 
   def index
@@ -72,6 +52,35 @@ class SupportsController < ApplicationController
   end
 
   private
+  def filter_state
+    case search_state
+    when 'done'
+      Support.done
+    when 'notdone'
+      Support.not_done
+    else
+      Support.all
+    end
+  end
+
+  def filter_input (supports_list)
+    return supports_list unless search_params.present?
+
+    if search_topic.present?
+      supports_list = supports_list.filter_by_topic(search_topic)
+    end
+    if search_problem.present?
+      supports_list  = supports_list.filter_by_problem(search_problem)
+    end
+    if search_receiver.present?
+      supports_list = supports_list.filter_by_receiver(search_receiver)
+    end
+    if search_user.present?
+      supports_list = supports_list.filter_by_user(search_user)
+    end
+
+    supports_list
+  end
 
   def support_params
     params.fetch(:support, {}).permit(:body, :user_id)
