0ee1c180ff546033d27066971c116f53d3932e5b
olgadraber <olgadraber@gmail.com>
2014-05-20 09:59:53 +0200

    Edited filtering scopes in support model

--- app/models/support.rb
+++ app/models/support.rb
@@ -9,10 +9,12 @@ class Support < ActiveRecord::Base
   scope :not_done, -> { where(done: false) }
   default_scope -> { order(updated_at: :desc) }
 
-  scope :filter_by_topic, -> (topic) { joins(:topic).where(topics: {title: topic} ) }
-  scope :filter_by_problem, -> (problem) { where(body: problem) }
-  scope :filter_by_receiver, -> (receiver) { joins(:receiver).where("first_name = ? OR last_name = ?", receiver, receiver ) }
-  scope :filter_by_user, -> (user) { joins(:user).where("first_name = ? OR last_name = ?", user, user ) }
+  scope :filter_by_topic, -> (topic) { joins(:topic).where(Topic.arel_table[:title].matches("%#{topic}%") ) }
+  scope :filter_by_problem, -> (problem) { where(Support.arel_table[:body].matches("%#{problem}%")) }
+  scope :filter_by_receiver, -> (receiver) { joins(:receiver).where(User.arel_table[:first_name].matches("%#{receiver}%").
+      or(User.arel_table[:last_name].matches("%#{receiver}%"))) }
+  scope :filter_by_user, -> (user) { joins(:user).where(User.arel_table[:first_name].matches("%#{user}%").
+      or(User.arel_table[:last_name].matches("%#{user}%"))) }
 
   def discussed?
     comments_count > 0
