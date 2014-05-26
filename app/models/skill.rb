1a0851787f15dbe2740bee351e77c09c4649435a
olgadraber <olgadraber@gmail.com>
2014-05-26 13:25:50 +0200

    Deleted unused scope in skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,6 +2,4 @@ class Skill < ActiveRecord::Base
   belongs_to :user, counter_cache: true
   belongs_to :topic, counter_cache: true
 
-  scope :active, -> { joins(:user).where("users.archived = ?", false) }
-
 end
