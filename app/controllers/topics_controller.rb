6c2ffc7dad31882cd62acd9fe5e0932730a1d53c
olgadraber <olgadraber@gmail.com>
2014-05-26 09:25:03 +0200

    Hid archved user on topic show page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   expose_decorated(:topics) { Topic.all }
   expose_decorated(:topic) { Topic.find(params[:id]) }
   expose_decorated(:random_supporter, decorator: UserDecorator) {
-    topic.users.without(current_user.object).sample
+    topic.users.not_archived.without(current_user.object).sample
   }
 
   def index
