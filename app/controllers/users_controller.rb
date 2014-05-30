8c0814ca46e3da9ba02c9e4fccbfa6d2e02d9f2e
olgadraber <olgadraber@gmail.com>
2014-05-30 13:23:45 +0200

    Change variable name to be more precise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   expose_decorated(:users) { User.sorted }
-  expose_decorated(:this_week_users, decorator: UserDecorator) { User.this_week_best_users }
+  expose_decorated(:this_week_best_users, decorator: UserDecorator) { User.this_week_best_users }
   expose_decorated(:user) { User.find params[:id] }
   expose_decorated(:supports) { user.supports.done }
   expose_decorated(:skills) { user.skills.includes(:topic) }
