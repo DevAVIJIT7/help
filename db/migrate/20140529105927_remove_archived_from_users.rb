ae5fad8036bdc717479188502e6e1cefac4e4872
olgadraber <olgadraber@gmail.com>
2014-05-30 11:56:25 +0200

    Edited migration

--- db/migrate/20140529105927_remove_archived_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveArchivedFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :archived, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529110953) do
+ActiveRecord::Schema.define(version: 20140530095202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,8 @@ ActiveRecord::Schema.define(version: 20140529110953) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "receiver_id"
-    t.boolean  "done",           default: false
+    t.boolean  "done",        default: false
     t.text     "body"
-    t.integer  "comments_count", default: 0
   end
 
   add_index "supports", ["receiver_id"], name: "index_supports_on_receiver_id", using: :btree
