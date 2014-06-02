6f8a11797b546b994b10a790558c6527f1519df4
Adam Nowak <adam.nowak@netguru.pl>
2014-06-02 16:37:36 +0200

    use scope instead of class method

--- app/models/support.rb
+++ app/models/support.rb
@@ -5,15 +5,14 @@ class Support < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic, counter_cache: true
 
+  default_scope -> { order(updated_at: :desc) }
   scope :done, -> { where(done: true) }
   scope :not_done, -> { where(done: false) }
-  default_scope -> { order(updated_at: :desc) }
+  scope :from_beginning_of_week, -> do
+    done.where('supports.updated_at >= ?', Time.current.beginning_of_week)
+  end
 
   def discussed?
     comments_count > 0
-  end
-
-  def self.from_beginning_of_week
-    done.where('supports.updated_at >= ?', Time.current.beginning_of_week)
   end
 end
