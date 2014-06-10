30820946e07025779f1f52b187ab3a97a8d6f8a2
Adam Nowak <adam.nowak@netguru.pl>
2014-06-10 20:50:56 +0200

    split long line

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -7,7 +7,8 @@ describe TopicsController do
   describe 'GET show' do
     context "when there's no users assigned for that topic" do
       before do
-        expect(controller).to receive(:current_user).and_return(user).at_least(:once)
+        expect(controller).to receive(:current_user)
+          .and_return(user).at_least(:once)
         expect(controller).to receive(:topic).and_return(topic).at_least(:once)
       end
 
