c73e5ab2f02341f374c4bd23e6f5fc1ba4756e7f
Adam Nowak <adam.nowak@netguru.pl>
2014-06-10 20:44:26 +0200

    redirect to topics index when there's no one available for help

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,10 +7,18 @@ class TopicsController < ApplicationController
     topic.users.without(current_user.object).sample
   }
 
+  before_filter :check_supporters_availability, only: :show
+
   def index
   end
 
   private
+
+  def check_supporters_availability
+    if random_supporter.nil?
+      redirect_to topics_path, alert: t('topic.no_supporters')
+    end
+  end
 
   def support_params
     params.fetch(:support, {}).permit(:body)
