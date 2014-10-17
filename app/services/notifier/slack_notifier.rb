3175a11b6c8a6a845873f3dcd1ea6d1b0f30b99f
Rafał Wardenga <rafal12121@gmail.com>
2014-10-17 15:31:13 +0200

    create slack notifier class
    [#79770248]

---
+++ app/services/notifier/slack_notifier.rb
@@ -0,0 +1,17 @@
+class Notifier
+  class SlackNotifier < Base
+
+    private
+
+    def notify
+      client.ping notification.to_s
+    end
+
+    def client
+      @client ||= ::Slack::Notifier.new AppConfig.slack.team,
+        AppConfig.slack.token,
+        channel: AppConfig.slack.default_channel,
+        username: 'HelpApp'
+    end
+  end
+end
