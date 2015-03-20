e6dfa94241c24cd780d0e35b617bcf015d98f3cf
Marek Mikołajczyk <marek.mikolajczyk@netguru.pl>
2015-03-20 21:20:54 +0100

    Adding authentication helper for integration tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,8 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
+  config.include Features::GoogleAuth, type: :feature
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
