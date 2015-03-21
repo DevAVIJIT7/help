b4af9a1573a98641ffbef348bef9ba8004a323d7
rudzikowa89 <marta.meczekalska@netguru.pl>
2015-03-21 17:43:54 +0100

    Skills selection spec

---
+++ spec/features/skills_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'skills page', js: true do
+
+	let(:user) { create :user }
+	let!(:topic1) { create :topic, title: 'workshops' }
+	let!(:topic2) { create :topic, title: 'mongo' }
+	
+	before do
+		authenticate_user(user)
+	end
+
+	it "doesn't highlight deselected topic" do
+		topic1.skills << create(:skill, user: user, topic: topic1)
+		visit skills_path
+		selector_css = "input.item.active[value='#{topic1.title} - you can help with that!']"
+		expect(page).to have_selector(selector_css)
+		find(selector_css).click
+		selector_css = "input.item[value='#{topic1.title} - you are not helping yet.']"
+		expect(page).to have_selector(selector_css)
+
+		expect(find(selector_css)[:class]).to_not include 'active'
+		expect(user.topics).to_not include(topic1) 
+	end
+end
