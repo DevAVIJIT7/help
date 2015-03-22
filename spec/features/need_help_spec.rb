7fb7ff94efa52808d0ccafb865baa7ab60aed47a
rudzikowa89 <marta.meczekalska@netguru.pl>
2015-03-22 17:34:03 +0100

    Updating need help specs

--- spec/features/need_help_spec.rb
+++ spec/features/need_help_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Creating help request', js: true do
+feature 'Creating help request', js: true do
   let(:user) { create :user }
   let!(:topic1) { create :topic, title: 'super help', description: 'Help me!' }
   let!(:supporter) { create :user }
@@ -8,10 +8,10 @@ describe 'Creating help request', js: true do
   before do
     authenticate_user(user)
     topic1.skills << create(:skill, user: supporter, topic: topic1)
-    visit root_path
   end
 
-  it 'adding help request' do
+  scenario 'adding help request' do
+    visit root_path
     click_on 'I need help'
     expect(page).to have_xpath('/html/body/main/div[2]/div[2]/div/a/span[3]')
     find("a[href='/topics/1']").click
@@ -19,7 +19,14 @@ describe 'Creating help request', js: true do
 
     fill_in 'support_body', with: 'HELP ME!'
     click_on 'Ask for help'
-    expect(page).to have_content('We asked to help you.')
+    expect(page).to have_content("We asked #{supporter.first_name} to help you.")
     expect(current_path).to eq(root_path)
+  end
+
+  scenario 'skills counter shows correct value' do
+    topic1.skills << create(:skill, topic: topic1)
+    visit topics_path
+    expect(page).not_to have_css('.item__count span', text: '1')
+    expect(page).to have_css('.item__count span', text: '2')
   end
 end
