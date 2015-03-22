d5ed75126b6e3f8274e18a2d7f2118a0fd01cf53
rudzikowa89 <marta.meczekalska@netguru.pl>
2015-03-22 10:26:17 +0100

    Fixing indentation

--- spec/features/skills_spec.rb
+++ spec/features/skills_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 
 describe 'skills page', js: true do
 
-	let(:user) { create :user }
-	let!(:topic1) { create :topic, title: 'workshops' }
-	let!(:topic2) { create :topic, title: 'mongo' }
-	
-	before do
-		authenticate_user(user)
-	end
+  let(:user) { create :user }
+  let!(:topic1) { create :topic, title: 'workshops' }
+  let!(:topic2) { create :topic, title: 'mongo' }
 
-	it "doesn't highlight deselected topic" do
-		topic1.skills << create(:skill, user: user, topic: topic1)
-		visit skills_path
-		selector_css = "input.item.active[value='#{topic1.title} - you can help with that!']"
-		expect(page).to have_selector(selector_css)
-		find(selector_css).click
-		selector_css = "input.item[value='#{topic1.title} - you are not helping yet.']"
-		expect(page).to have_selector(selector_css)
+  before do
+    authenticate_user(user)
+  end
 
-		expect(find(selector_css)[:class]).to_not include 'active'
-		expect(user.topics).to_not include(topic1) 
-	end
+  it "doesn't highlight deselected topic" do
+    topic1.skills << create(:skill, user: user, topic: topic1)
+    visit skills_path
+    selector_css = "input.item.active[value='#{topic1.title} - you can help with that!']"
+    expect(page).to have_selector(selector_css)
+    find(selector_css).click
+    selector_css = "input.item[value='#{topic1.title} - you are not helping yet.']"
+    expect(page).to have_selector(selector_css)
+
+    expect(find(selector_css)[:class]).to_not include 'active'
+    expect(user.topics).to_not include(topic1)
+  end
 
   it 'highlights selected topic' do
     visit skills_path
