10c195d6901424a8ec50e579d66af62a9b9c62d2
DeeDee88 <dorota.nieweglowska@o2.pl>
2015-03-22 13:27:26 +0100

    Updating user and support factories

--- spec/factories/support_factory.rb
+++ spec/factories/support_factory.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :support, class: Support do
     user
     topic
+    body Forgery(:lorem_ipsum).words(10)
+  end
+
+  factory :done_support, parent: :support do
+    done true
   end
 end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,21 @@
 FactoryGirl.define do
 
   factory :user, class: User do
+    first_name Forgery::Name.first_name
     sequence(:email) { |n| "user#{n}@email.com" }
+  end
+
+  factory :user_with_supports, parent: :user do
+
+    ignore do
+      supports_count 1
+    end
+
+    after(:create) do |user, evaluator|
+      evaluator.supports_count.times do
+        create(:done_support, user: user)
+      end
+    end
   end
 
 end
